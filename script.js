44f024aaf2b403938e47a0b69de192776d9588f0
soujanya gupta <semrisoujanya2003@gmail.com>
2024-02-03 20:38:12 +0530

    add new style

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let count = document.getElementById("counter");
 let value = 0;
+count.style.color = "white"
 
 function DecreaseCounter() {
     value = value - 1;
     // value.toString();
     count.innerHTML = value;
+    if (value > 0) {
+        count.style.color = "green"
+    } else if (value < 0) {
+        count.style.color = "yellow"
+    } else {
+        count.style.color = "white"
+    }
 }
 
 function IncreseCounter() {
     value = value + 1;
     count.innerHTML = value;
+    if (value > 0) {
+        count.style.color = "green"
+    } else if (value < 0) {
+        count.style.color = "yellow"
+    } else {
+        count.style.color = "white"
+    }
 }
